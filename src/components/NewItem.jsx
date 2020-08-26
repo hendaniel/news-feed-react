d4bd7718f2bb06b64ddee205c16595f63815c2e2
Daniel Martinez <danielmartinezdm@hotmail.com>
2020-08-26 10:14:16 -0500

    Se ajustan estilos del menu

--- src/components/NewItem.jsx
+++ src/components/NewItem.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import no_new_image from "../assets/blank_image.jpg";
 
 const NewItem = ({ item }) => {
   const { date, img_url, url } = item;
   console.log(img_url);
   return (
-    <div className="news-card" style={{ backgroundImage: `url("${img_url}")` }}>
+    <div className="news-card" style={{ backgroundImage: `url("${img_url || no_new_image}")` }}>
       <div className="new-container">
         <div className="content">
           <button class="btn" onClick={() => window.open(url, "_blank")}>
