4163d6a9aa247556d133d3c2d0b41f474b31da0e
Daniel Martinez <danielmartinezdm@hotmail.com>
2020-08-27 11:29:02 -0500

    Updates

---
+++ precache-manifest.f44c4c8e00d07cde31582c94afe5dd2c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108eabc8c2a2d67e786e2e3674d23702",
+    "url": "/NewsFeedReact/index.html"
+  },
+  {
+    "revision": "b2eb39ccf50c2b3b385c",
+    "url": "/NewsFeedReact/static/css/main.7f9da577.chunk.css"
+  },
+  {
+    "revision": "e5ca8e7e5345f49310b0",
+    "url": "/NewsFeedReact/static/js/2.e8625908.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/NewsFeedReact/static/js/2.e8625908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2eb39ccf50c2b3b385c",
+    "url": "/NewsFeedReact/static/js/main.bb0a6e33.chunk.js"
+  },
+  {
+    "revision": "3a03300532287e42ced3",
+    "url": "/NewsFeedReact/static/js/runtime-main.eac3e574.js"
+  },
+  {
+    "revision": "da01e787e70662b72949ff4a72668ebe",
+    "url": "/NewsFeedReact/static/media/blank_image.da01e787.jpg"
+  },
+  {
+    "revision": "e4b6323e19e57f6761b1ce47b7d329e0",
+    "url": "/NewsFeedReact/static/media/loader.e4b6323e.gif"
+  }
+]);
