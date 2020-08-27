fce6fc6681c391e7fb6ad7b98f1d3716797983d5
Daniel Martinez <danielmartinezdm@hotmail.com>
2020-08-27 11:48:15 -0500

    Se usa hashrouter para evitar conflicto con pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import storeFactory from "./store";
 import App from "./components/App";
@@ -9,10 +9,10 @@ import App from "./components/App";
 const store = storeFactory();
 
 render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
